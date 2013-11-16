6e2555fdd9dd9aaa3761a13c56d900ce02f802b2
Joe Meirow <joe.meirow@gmail.com>
2013-11-15 19:50:48 -0500

    initial version

---
+++ ruby_class/chapter_01_intro/ex_04_parens_defining_and_calling_methods.rb
@@ -0,0 +1,44 @@
+#######################################################################################
+## 
+##    Chapter  1 - Introduction to Ruby
+##    Exercise 4 - Parenthses - defining and calling methods
+##
+##
+##    For this exercise you will run the file from the command line:
+##   
+##    $ ruby ex_04_parens_defining_and_calling_methods.rb
+##
+#########################################################################################
+
+
+
+#########################################################################################
+# defining and calling methods
+#########################################################################################
+
+
+def add arg1, arg2
+  arg1 + arg2
+end
+
+
+def subtract (arg1, arg2)
+  arg1 - arg2
+end
+
+
+puts add 1, 4
+
+puts add(2, 5)
+
+puts subtract 19, 4
+
+puts subtract(20, 7)
+
+
+
+
+
+
+
+
